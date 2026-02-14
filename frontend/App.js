fa52a99e30367011451cf645f77b84901bfc59b5
Gameguru <madhurgupt10@gmail.com>
2026-02-14 17:08:20 +0530

    Initialize App component with GameScreen

---
+++ frontend/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import GameScreen from './screens/GameScreen';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <GameScreen />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+});
